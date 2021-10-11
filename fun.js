b92e8fc9c60e530a42bd249cdc088c4a2f5673ea
Jonas Almeida <jalmeida@mathbiol.org>
2021-10-11 12:27:38 -0400

    importing without await

---
+++ fun.js
@@ -0,0 +1,4 @@
+if (typeof (define) != 'undefined') {
+    var fun = function(){return ':-)'}
+    define(fun)
+}
--- img3.js
+++ img3.js
@@ -2,7 +2,22 @@ console.log('img3.js loaded');
 
 // loading ES6 module for script tag
 
+/*
 (async function(){
+    //debugger
     //console.log('loading from module img3Mod.js');
-    img3 = await import('./img3Mod.js')
+    img3 = await import('https://episphere.github.io/img3/img3Mod.js')
 })()
+
+if (typeof (define) != 'undefined') {
+    var fun = function(){return ':-)'}
+    define(fun)
+}
+*/
+
+img3 = import('https://episphere.github.io/img3/img3Mod.js')
+
+if (typeof (define) != 'undefined') {
+    var fun = function(){return ':-)'}
+    define(img3)
+}
