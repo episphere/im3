7cb364b3ca57bc87f902741aec6d2a0837f3f0c7
Jonas Almeida <jalmeida@mathbiol.org>
2021-10-12 11:21:14 -0400

    keeping an eye on the good old script tag model

---
+++ img3old.js
@@ -0,0 +1,21 @@
+img3old={}
+
+img3old.img2Read=function(imgUrl='https://storage.googleapis.com/imagebox_test/Slide-0027830_Y561170_1002408.svs',xPos=30720,yPos=6144,resol=512){ // read with imageBox2 tile service
+    const url = `https://imageboxv2-oxxe7c4jbq-uc.a.run.app/iiif/?iiif=${imgUrl}/${xPos},${yPos},1024,1024/${resol},/0/default.jpg`;
+    let im = document.createElement('img')
+    im.src=url
+    return im
+}
+
+img3old.imgReadData=function(img){ // read image data
+    //document.body.appendChild(img)
+    let cv = document.createElement('canvas')
+    //document.body.appendChild(cv)
+    cv.width=img.width
+    cv.height=img.height
+    let ctx=cv.getContext('2d')
+    //img.crossOrigin = "Anonymous"
+    ctx.drawImage(img,0,0)
+    return ctx.getImageData(0,0,cv.width,cv.height)
+    //return ctx
+}
