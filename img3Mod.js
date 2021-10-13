e3ae54957537af07444b8adceae6fce2e81ce1f2
Jonas Almeida <jalmeida@mathbiol.org>
2021-10-13 14:03:27 -0400

    anonymous image needs to be set before sourcing

--- img3Mod.js
+++ img3Mod.js
@@ -1,6 +1,7 @@
 function img2Read(imgUrl='https://storage.googleapis.com/imagebox_test/Slide-0027830_Y561170_1002408.svs',xPos=30720,yPos=6144,resol=512){ // read with imageBox2 tile service
     const url = `https://imageboxv2-oxxe7c4jbq-uc.a.run.app/iiif/?iiif=${imgUrl}/${xPos},${yPos},1024,1024/${resol},/0/default.jpg`;
     let im = document.createElement('img')
+    im.crossOrigin = "Anonymous"
     im.src=url
     return im
 }
@@ -12,12 +13,13 @@ function imgReadData(img){ // read image data
     cv.width=img.width
     cv.height=img.height
     let ctx=cv.getContext('2d')
-    //img.crossOrigin = "Anonymous"
     ctx.drawImage(img,0,0)
     return ctx.getImageData(0,0,cv.width,cv.height)
     //return ctx
 }
 
-export {img2Read,imgReadData}
+const lala=':-O'
+
+export {img2Read,imgReadData,lala}
 
 
