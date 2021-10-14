afae7a4819c706ee9481c52e00964155d51ad7a8
Jonas Almeida <jalmeida@mathbiol.org>
2021-10-14 12:57:49 -0400

    fully load image before returning

--- img3Mod.js
+++ img3Mod.js
@@ -1,8 +1,13 @@
-function img2Read(imgUrl='https://storage.googleapis.com/imagebox_test/Slide-0027830_Y561170_1002408.svs',xPos=30720,yPos=6144,resol=512){ // read with imageBox2 tile service
+async function img2Read(imgUrl='https://storage.googleapis.com/imagebox_test/Slide-0027830_Y561170_1002408.svs',xPos=30720,yPos=6144,resol=512){ // read with imageBox2 tile service
     const url = `https://imageboxv2-oxxe7c4jbq-uc.a.run.app/iiif/?iiif=${imgUrl}/${xPos},${yPos},1024,1024/${resol},/0/default.jpg`;
     let im = document.createElement('img')
     im.crossOrigin = "Anonymous"
     im.src=url
+    let imgPromise = new Promise((resolve, reject) => {
+        //debugger
+        im.onload=resolve
+    })
+    await imgPromise
     return im
 }
 
@@ -18,8 +23,26 @@ function imgReadData(img){ // read image data
     //return ctx
 }
 
-const lala=':-O'
+function range(n){ // create a range vector [0,...,n-1]
+    return [...Array(n)].map((_,i)=>i)
+}
 
-export {img2Read,imgReadData,lala}
+function imgData2Array(imData){
+    // extract rgba quads
+    let rgba = range(imData.data.length/4).map(i=>imData.data.slice(i*4,i*4+4))
+    // fold into a 2d array
+
+    /*
+    let 2dArr=([...Array(imData.height)].map((_,i)=>i)).map(i=>{
+        return ([...Array(imData.width)].map((_,j)=>j)).map(j=>{
+            return imData.data.slice(i*imData.width,i*imData.width+j*imData.height)
+        })
+    })
+    return 2dArr
+    */
+}
+
+
+export {img2Read,imgReadData,imgData2Array}
 
 
